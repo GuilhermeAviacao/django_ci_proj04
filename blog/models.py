eb83427932ef72f9e5bf27b088ea47dd4479587c
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-05-07 22:40:00 +0200

    Added class Meta

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User # Import models to connect
+from django.contrib.auth.models import User  # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
 class Post(models.Model):
@@ -16,6 +17,13 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -24,3 +32,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
